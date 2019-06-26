515425725313485678d5274669c199e05ffaefb7
mona <roessramona@gmail.com>
2019-06-26 19:28:39 +0900

    Set port for use with Docker

--- restart-server.sh
+++ restart-server.sh
@@ -1,5 +1,5 @@
 ./node_modules/forever/bin/forever stop server.js
-./node_modules/forever/bin/forever start -o server.log server.js /var/www/html http://localhost 8080
+./node_modules/forever/bin/forever start -o server.log server.js /var/www/html http://localhost:8010 8080
 sleep 1
 echo -e "\n\n# Ctrl+C to quit. This server will be still running.\n\n"
 tail -f server.log
