9ad2e61541686182ebd155213d7e3cdb985fbe22
mona <roessramona@gmail.com>
2019-09-30 22:18:17 +0200

    Make writing tabs in textareas work

--- src/index.js
+++ src/index.js
@@ -88,18 +88,18 @@ $(function () {
       $('img#logo').attr('src', './img/g2g_static.png');
     })
   });
+  $('textarea').on('keydown', function (e) {
+    if (e.keyCode === 9) {
+      e.preventDefault();
+      var elem = e.target;
+      var val = elem.value;
+      var pos = elem.selectionStart;
+      elem.value = val.substr(0, pos) + '\t' + val.substr(pos, val.length);
+      elem.setSelectionRange(pos + 1, pos + 1);
+    }
+  });
 });
 
-$('textarea').on('keydown', function (e) {
-  if (e.keyCode === 9) {
-    e.preventDefault();
-    var elem = e.target;
-    var val = elem.value;
-    var pos = elem.selectionStart;
-    elem.value = val.substr(0, pos) + '\t' + val.substr(pos, val.length);
-    elem.setSelectionRange(pos + 1, pos + 1);
-  }
-});
 
 var clear_output = function (callback) {
   $('#pg').val('');
