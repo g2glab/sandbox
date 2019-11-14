d7a449d395ed79a966109cd4cb63a417405ab3d0
Anton Lydike <me@AntonLydike.de>
2019-11-14 15:32:34 +0100

    simplify server.js and get cors middleware running

--- src/server.js
+++ src/server.js
@@ -1,48 +1,45 @@
-// $ export G2GSANDBOX_EXTERNAL_HOST="http://localhost"
-// $ export G2GSANDBOX_EXTERNAL_PORT="8080"
-// $ node server.js
-
-// sandbox listening config
-var g2gsandbox_host = "http://localhost";
-if (process.env.G2GSANDBOX_EXTERNAL_HOST != null) {
-  g2gsandbox_host = process.env.G2GSANDBOX_EXTERNAL_HOST
-};
-var g2gsandbox_port = "8080";
-if (process.env.G2GSANDBOX_EXTERNAL_PORT != null) {
-  g2gsandbox_port = process.env.G2GSANDBOX_EXTERNAL_PORT
-};
+// get sandbox listening config from environment
+const g2gsandbox_host = process.env.G2GSANDBOX_EXTERNAL_HOST || "http://localhost";
+const g2gsandbox_port = process.env.G2GSANDBOX_EXTERNAL_PORT || "8080";
 const g2gsandbox_external_url = g2gsandbox_host + ":" + g2gsandbox_port;
 
-var bodyParser = require('body-parser');
-var childProcess = require('child_process');
-var fs = require('fs');
-var express = require('express');
-var mkdirp = require('mkdirp');
-var rimraf = require('rimraf');
-const exec = childProcess.exec;
+// get required packages
+const bodyParser = require('body-parser');
+const childProcess = require('child_process');
+const fs = require('fs');
+const express = require('express');
+const mkdirp = require('mkdirp');
 
-// WEB SERVER
+// declare some helper functions
+const exec = childProcess.exec;
+const returnResult = function (res, body) {
+  res.set('Content-Type', 'application/json');
+  res.json(body);
+};
+
+////////////////
+// WEB SERVER //
+////////////////
 var app = express();
+
+// configure middlewares
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+  // allow pre-flight cors requests
+  if (req.method === 'OPTIONS') {
+    res.send(200);
+  } else {
+    next();
+  }
+});
 app.use(express.static('static'));
-app.use(express.static('src'));
+app.use('/src', express.static('src'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-app.listen(g2gsandbox_port);
-console.log('Listening port is ' + g2gsandbox_port);
 
-app.get('/', function () {
-  returnResult(res, "index.html")
-});
-
-app.get('/index.js', function () { 
-  returnResult(res, "src/index.js")
-});
-
+// this is our endpoint
 app.post('/g2g/', function (req, res) {
   // g2g output file path configuration
   var id = new Date().getTime();
@@ -81,7 +78,6 @@ app.post('/g2g/', function (req, res) {
   });
 });
 
-var returnResult = function (res, body) {
-  res.set('Content-Type', 'application/json');
-  res.json(body);
-};
+// start listening
+app.listen(g2gsandbox_port);
+console.log('Listening port is ' + g2gsandbox_port);
